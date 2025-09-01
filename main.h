26060b8b875a1ef4378bc09269f7cdad70d8ae50
Hashimo77 <hagamirz7@gmail.com>
2025-09-01 10:05:14 +0000

    Task 1

--- main.h
+++ main.h
@@ -4,10 +4,8 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/* Prototype for _printf */
 int _printf(const char *format, ...);
-/* Prototype for handling single specifier */
-int handle_specifier(char c, va_list args);
+int print_number(int n);
 
 #endif /* MAIN_H */
 
