3176f6c83dd5b328b187fbfcbced6e0249825fae
Hashimo77 <hagamirz7@gmail.com>
2025-09-02 09:32:19 +0000

    Task 1

--- _printf.c
+++ _printf.c
@@ -1,37 +1,55 @@
 #include "main.h"
 
 /**
- * _printf - prints according to a format
- * @format: the format string
+ * _printf - produces output according to a format
+ * @format: character string with format specifiers
  *
- * Return: number of characters printed, or -1 if format is NULL
+ * Return: number of characters printed (excluding null byte)
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0, count = 0;
+	char *str;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
 
-	while (format[i] != '\0')
+	while (format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
 			if (format[i] == '\0')
+				break;
+
+			if (format[i] == 'c')
+				count += _putchar(va_arg(args, int));
+			else if (format[i] == 's')
 			{
-				va_end(args);
-				return (-1);
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+				{
+					_putchar(*str);
+					str++;
+					count++;
+				}
 			}
-			count += handle_specifier(format[i], args);
+			else if (format[i] == '%')
+				count += _putchar('%');
+			else
+			{
+				count += _putchar('%');
+				count += _putchar(format[i]);
+			}
 		}
 		else
 		{
-			write(1, &format[i], 1);
-			count++;
+			count += _putchar(format[i]);
 		}
 		i++;
 	}
--- main.h
+++ main.h
@@ -4,11 +4,8 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/* Prototype for _printf */
 int _printf(const char *format, ...);
-
-/* Prototype for handling a single format specifier */
-int handle_specifier(char c, va_list args);
+int _putchar(char c);
 
 #endif /* MAIN_H */
 
